cc97036f7d539272e36c23ecaebee7b18d3c6dc8
nahuel-coliva <nahuelcoliva@hotmail.it>
2026-07-20 11:28:14 +0200

    Fix Maven wrapper startup

--- backend/mvnw.cmd
+++ backend/mvnw.cmd
@@ -5,11 +5,11 @@ set BASEDIR=%~dp0
 set WRAPPER_DIR=%BASEDIR%.mvn\wrapper
 set WRAPPER_JAR=%WRAPPER_DIR%\maven-wrapper.jar
 set WRAPPER_PROPS=%WRAPPER_DIR%\maven-wrapper.properties
+set MAVEN_PROJECTBASEDIR=%BASEDIR:~0,-1%
 
 if not exist "%WRAPPER_JAR%" (
   powershell -NoProfile -ExecutionPolicy Bypass -Command "$props = Get-Content '%WRAPPER_PROPS%'; $url = ($props | Where-Object { $_ -like 'wrapperUrl=*' }) -replace 'wrapperUrl=', ''; New-Item -ItemType Directory -Force -Path '%WRAPPER_DIR%' | Out-Null; Invoke-WebRequest -Uri $url -OutFile '%WRAPPER_JAR%'"
   if errorlevel 1 exit /b 1
 )
 
-java -jar "%WRAPPER_JAR%" %*
-
+java "-Dmaven.multiModuleProjectDirectory=%MAVEN_PROJECTBASEDIR%" -classpath "%WRAPPER_JAR%" org.apache.maven.wrapper.MavenWrapperMain %*
